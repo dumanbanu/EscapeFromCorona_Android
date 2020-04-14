2a54062d68aa4f66eba3953818341445ade1b9d5
Banu Duman <banu.duman@santsg.com>
2020-04-14 18:17:15 +0300

    Project

---
+++ EscapeFromCorona/android/src/com/mobilprogramming/escapecorona/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.mobilprogramming.escapecorona;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mobilprogramming.escapecorona.EscapeFromCorona;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new EscapeFromCorona(), config);
+	}
+}
